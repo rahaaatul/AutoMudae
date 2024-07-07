c108a44165d4eeb4ab66f9bf89a453da95af422c
Rahatul Ghazi <86149861+rahaaatul@users.noreply.github.com>
2024-07-07 19:53:43 +0600

    Delete Vars.py

--- Vars.py
+++
@@ -1,9 +0,0 @@
-token = ''  
-channelId = ''                 
-serverId = ''         
-
-rollCommand= 'wa'
-desiredKakeras = ['kakeraP','kakeraY','kakeraO','kakeraR','kakeraW','kakeraL']
-desiredSeries = ['One Piece','Dragon Ball Z','Death Note']
-pokeRoll = True
-repeatMinute = '25'
